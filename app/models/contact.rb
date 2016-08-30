d9656d4a6eda8ad9fad2eb564299f103c9b7dee5
Ryan Everett <reverett4994@gmail.com>
2016-08-30 04:43:41 -0400

    added sendgrip mailer 2

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,7 +11,7 @@ class Contact <MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "Subject?",
+      :subject => "Welcome",
       :to => "pattonsrevolver@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
